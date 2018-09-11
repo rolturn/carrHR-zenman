17c9df53ab20f4ac5337176af2c89aa29e107810
rolturn <rol.turn@gmail.com>
2018-09-10 20:17:58 -0500

    extending video to plan on page

--- single.php
+++ single.php
@@ -114,3 +114,36 @@ if ($series_count > 1) {
 <?php endwhile; // end of the loop. ?>
 
 <?php get_footer(); ?>
+
+<script>
+  // Find all YouTube videos
+  var $allVideos = $("iframe[src^='//www.youtube.com']");
+
+  // The element that is fluid width
+  var $fluidEl = $(".post__article");
+
+  // Figure out and save aspect ratio for each video
+  $allVideos.each(function() {
+
+    $(this)
+      .attr('data-ratio', this.height / this.width)
+      // and remove the hard coded width/height
+      .removeAttr('height').removeAttr('width')
+      // wrap in div for absolute positioning
+      .wrap("<div class='video-wrapper'></div>");
+  });
+
+  // When the window is resized
+  $(window).resize(function() {
+
+    var newWidth = $fluidEl.width();
+
+    // Resize all videos according to their own aspect ratio
+    $allVideos.each(function() {
+      $(this).width(newWidth).height(newWidth * $(this).data('ratio'));
+    });
+
+  // Kick off one resize to fix all videos on page load
+  }).resize();
+
+</script>
